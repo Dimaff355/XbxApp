3298e98fc04bc32b9336a880b58ef5d7b17829fd
Дмитрий <dkovalevskij@yandex.ru>
2021-08-18 16:32:22 +0300

    тест

--- app/src/main/java/com/example/xbxapp/MainActivity.kt
+++ app/src/main/java/com/example/xbxapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.xbxapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.xbxapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bFrag2.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.place_holder, BlankFragment2.newInstance())
+                .commit()
+        }
+
+        supportFragmentManager
+            .beginTransaction() // запуск фрагмента
+            .replace(R.id.place_holder, MainFragment.newInstance()) // заменяет фрагмент если он уже есть
+            .commit() // активирует всё чтоб заработало
     }
 }
